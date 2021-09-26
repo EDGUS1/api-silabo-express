ce5c6ae5a8cfdfac829d0dc3def5152cf37ea6da
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-26 11:43:16 -0500

    chore: add path lib

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const loginRoute = require('./routes/login.routes');
 const silaboRoute = require('./routes/silabo.routes');
