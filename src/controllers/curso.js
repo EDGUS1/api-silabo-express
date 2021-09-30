03ba9af07074322e1933b36dd0cf29d52042c73a
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-30 12:50:51 -0500

    feat: add save course route

--- src/controllers/curso.js
+++ src/controllers/curso.js
@@ -10,4 +10,36 @@ controller.listar = async (req, res) => {
   }
 };
 
+controller.save = async (req, res) => {
+  try {
+    const {
+      codigo,
+      nombre,
+      tipo,
+      horas,
+      ciclo,
+      creditos,
+      sumilla,
+      estrategia,
+      plan,
+    } = req.body;
+    const query =
+      'insert into asignatura (asig_codigo, asig_nombre, horas_sem_id, asig_ciclo, asig_sumilla, asig_creditos, asig_estrategia_didactica, tipo_asignatura_id, plan_id) values (?,?,?,?,?,?,?,?,?)';
+    const response = await pool.query(query, [
+      codigo,
+      nombre,
+      horas,
+      ciclo,
+      sumilla,
+      creditos,
+      estrategia,
+      tipo,
+      plan,
+    ]);
+    res.json(response);
+  } catch (e) {
+    res.status(501).json({ error: e });
+  }
+};
+
 module.exports = controller;
--- src/routes/curso.routes.js
+++ src/routes/curso.routes.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/curso');
 const router = express.Router();
 
 router.get('/', controller.listar);
+router.post('/', controller.save);
 
 module.exports = router;
