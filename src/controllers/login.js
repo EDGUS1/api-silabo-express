583ce63b63a45c861f999b8f7073342dbe9733b5
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-10-02 15:11:07 -0500

    chore: add password hash

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,27 +1,34 @@
 const pool = require('../database');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const controller = {};
 
 controller.login = async (req, res) => {
   const { email, password } = req.body;
-  /* console.log(req.ip);
-  console.log(req.ips);
-  console.log(req.originalUrl); */
+
   const respuesta = await pool.query(
-    'select * from usuario where usuario_email = ? and usuario_password = ?',
-    [email, password]
+    'select * from usuario where usuario_email = ? ',
+    [email]
   );
 
-  if (!respuesta[0]) return res.json({ error: 'Not found' });
+  const passwordCorrect = !respuesta[0]
+    ? false
+    : await bcrypt.compare(password, respuesta[0].usuario_password);
+
+  if (!(respuesta[0] && passwordCorrect))
+    return res.json({ error: 'Invalid user or password' });
 
   const userForToken = {
     id: respuesta[0].usuario_id,
     email: respuesta[0].usuario_email,
   };
 
-  const token = jwt.sign(userForToken, process.env.JWT_SECRET);
+  const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60,
+  });
+
   res.send({
     user_email: respuesta[0].usuario_email,
     token,
@@ -30,11 +37,13 @@ controller.login = async (req, res) => {
 
 controller.register = async (req, res) => {
   const { email, password } = req.body;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
   const respuesta = await pool.query(
     'insert into usuario (usuario_email,usuario_password) values (?, ?)',
-    [email, password]
+    [email, passwordHash]
   );
-  res.json(respuesta);
+  res.status(201).json(respuesta);
 };
 
 controller.listar = async (req, res) => {
