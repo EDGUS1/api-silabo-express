1c4f4cdf3f72ec99720656bdf0e3cea1c06dabe7
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-10-08 17:57:51 -0500

    feat: add and fix competencia endpoint

--- src/controllers/competencia.js
+++ src/controllers/competencia.js
@@ -2,10 +2,18 @@ const pool = require('../database');
 
 const controller = {};
 
-controller.list = async (req, res) => {
+controller.especifica = async (req, res) => {
   const { id } = req.params;
   const query =
-    'select * from competencia_especifica ce left join asignatura asg on asg.plan_id = ce.plan_id where asig_id = ?';
+    'select ce.comp_esp_id, ce.comp_esp_codigo, ce.comp_esp_nombre, ce.plan_id from competencia_especifica ce left join asignatura asg on asg.plan_id = ce.plan_id where asg.asig_id = ?';
+  const competencias = await pool.query(query, [id]);
+  res.json(competencias);
+};
+
+controller.general = async (req, res) => {
+  const { id } = req.params;
+  const query =
+    'select cg.comp_gnral_id, cg.comp_gnral_codigo, cg.comp_gnral_nombre, cg.plan_id from competencia_general cg left join asignatura asg on asg.plan_id = cg.plan_id where asg.asig_id = ?';
   const competencias = await pool.query(query, [id]);
   res.json(competencias);
 };
--- src/routes/competencia.routes.js
+++ src/routes/competencia.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/competencia');
 const router = express.Router();
 
-router.get('/', controller.list);
+router.get('/especifica/:id', controller.especifica);
+router.get('/general/:id', controller.general);
 
 module.exports = router;
