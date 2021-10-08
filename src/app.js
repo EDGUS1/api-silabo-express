4a1b345fbdacb53c8ac7d002568a94107a1b17fd
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-10-08 18:51:10 -0500

    chore: delete /

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://silaboangular.netlify.app/',
+    origin: 'https://silaboangular.netlify.app',
   })
 );
 app.use(express.json());
