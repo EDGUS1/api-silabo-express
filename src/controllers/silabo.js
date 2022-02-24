eb5302b995e2473cc1bbede6d91b1bafed802f1a
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-02-23 21:43:17 -0500

    feat: Se agrego la referncia a un silabo

--- src/controllers/silabo.js
+++ src/controllers/silabo.js
@@ -13,7 +13,7 @@ const controller = {};
 controller.allbyid = async (req, res) => {
   const { id } = req.params;
   const query =
-    'SELECT asg.asig_id, asg.asig_nombre, asg.asig_ciclo, asg.asig_codigo, asg.asig_creditos, asg.asig_estrategia_didactica, asg.horas_sem_id, asg.plan_id, asg.tipo_asignatura_id, asp.periodo_academico, asp.asig_periodo_modalidad, asg.asig_sumilla, asu.updated_at, asu.asig_periodo_id, asu.favorito from asignatura_usuario asu LEFT JOIN asignatura_periodo asp ON asu.asig_periodo_id = asp.asig_periodo_id LEFT JOIN asignatura asg ON asp.asig_id = asg.asig_id WHERE asu.usuario_id = ?';
+    'SELECT asg.asig_id, asg.asig_nombre, asg.asig_ciclo, asg.asig_codigo, asg.asig_creditos, asg.asig_estrategia_didactica, asg.horas_sem_id, asg.plan_id, asg.tipo_asignatura_id, asp.periodo_academico, asp.asig_periodo_modalidad, asg.asig_sumilla, asu.updated_at, asu.asig_periodo_id, asu.favorito from asignatura_usuario asu LEFT JOIN asignatura_periodo asp ON asu.asig_periodo_id = asp.asig_periodo_id LEFT JOIN asignatura asg ON asp.asig_id = asg.asig_id WHERE asu.usuario_id = ? order by asig_periodo_id desc';
   const respuesta = await pool.query(query, [id]);
 
   const silabos = respuesta.map(r => ({
@@ -48,6 +48,7 @@ controller.save = async (req, res) => {
     docentes,
     competencias,
     capacidades,
+    referencias,
   } = req.body;
 
   const query_asignatura_periodo =
@@ -97,6 +98,40 @@ controller.save = async (req, res) => {
     ]);
   }
 
+  for (let i = 0; i < referencias.length; i++) {
+    const response_editorial = await pool.query(
+      'INSERT INTO editorial (editorial_nombre) values (?)',
+      [referencias[i].editorial_nombre]
+    );
+
+    const reponse_libro = await pool.query(
+      'INSERT INTO libro (libro_nombre, libro_fecha, editorial_id ,edicion) values (?,?,?,?)',
+      [
+        referencias[i].libro_nombre,
+        referencias[i].libro_fecha,
+        response_editorial.insertId,
+        referencias[i].edicion,
+      ]
+    );
+
+    await pool.query(
+      'INSERT INTO referencia (libro_id, asig_periodo_id) values (?,?)',
+      [reponse_libro.insertId, respuesta.insertId]
+    );
+
+    for (let j = 0; j < referencias[i].autores.length; j++) {
+      const response_autor = await pool.query(
+        'INSERT INTO autor (autor_nombre, autor_apellido_paterno) values (?,?)',
+        [referencias[i].autores[j].nombre, referencias[i].autores[j].apellido]
+      );
+
+      await pool.query(
+        'INSERT INTO libro_autor (libro_id, autor_id) values (?,?)',
+        [reponse_libro.insertId, response_autor.insertId]
+      );
+    }
+  }
+
   res.status(201).json(respuesta2);
 };
 
@@ -114,12 +149,15 @@ controller.pdf = async (req, res) => {
     'SELECT * FROM competencia_general cg inner join plan p on p.plan_id = cg.plan_id inner join asignatura a on a.plan_id = p.plan_id inner join asignatura_periodo ap on ap.asig_id = a.asig_id where ap.asig_periodo_id = ?';
   const query_especifico =
     'SELECT * FROM competencia_especifica ce inner join competencia_asignada ca on ce.comp_esp_id = ca.comp_esp_id where ca.asig_periodo_id = ?';
+  const query_referencia =
+    "SELECT l.libro_nombre, l.libro_fecha, l.edicion, e.editorial_nombre, (select GROUP_CONCAT(a.autor_nombre, ' ' ,a.autor_apellido_paterno SEPARATOR ', ') from autor a inner join libro_autor la on la.autor_id = a.autor_id where la.libro_id = l.libro_id) as nombre from referencia r inner join libro l on r.libro_id = l.libro_id inner join editorial e on e.editorial_id = l.editorial_id where r.asig_periodo_id = ?";
 
   const respuesta = await pool.query(query, [silabo_id]);
   const respuesta_docente = await pool.query(query_docentes, [silabo_id]);
   const respuesta_capacidad = await pool.query(query_capacidad, [silabo_id]);
   const respuesta_general = await pool.query(query_general, [silabo_id]);
   const respuesta_especifico = await pool.query(query_especifico, [silabo_id]);
+  const respuesta_referencia = await pool.query(query_referencia, [silabo_id]);
 
   generatePdf(
     {
@@ -128,6 +166,7 @@ controller.pdf = async (req, res) => {
       capacidades: respuesta_capacidad,
       cgenerales: respuesta_general,
       cespecificos: respuesta_especifico,
+      referencias: respuesta_referencia,
     },
     response => {
       res.setHeader('Content-Type', 'application/pdf');
